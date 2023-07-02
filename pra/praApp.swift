6767f4f882daf67641c8f115f826c05d0c2a70bc
Kento Oda <kento.oda@relic.co.jp>
2023-07-02 16:36:35 +0900

    Initial Commit

---
+++ pra/praApp.swift
@@ -0,0 +1,17 @@
+//
+//  praApp.swift
+//  pra
+//
+//  Created by ODA KENTO on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct praApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
